2b3d54c23ce55e8ca766f6242e056ec142ebe444
Niv Sardi <xaiki@evilgiggle.com>
2022-08-05 17:09:12 -0300

    init
    Signed-off-by: Niv Sardi <xaiki@evilgiggle.com>

---
+++ detect.js
@@ -0,0 +1,23 @@
+// Set up a mutation observer to listen for title changes
+// Will fire if framework AJAX stuff switches page title
+let createObserver = function() {
+    let observer = new MutationObserver((mutations) => {
+        // Disconnect the MO so there isn't an infinite title update loop
+        // Run title cleanup again
+        // Create a new MO to listen for more changes
+        console.log('Mutations!', mutations)
+        observer.disconnect()
+        observer = null
+        cleanup()
+        createObserver()
+    })
+
+    observer.observe(
+        document.querySelector('input'),
+        { subtree: true, characterData: true, childList: true }
+    )
+}
+createObserver()
+
+// Kick off initial page load check
+cleanup()
