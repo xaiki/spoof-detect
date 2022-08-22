6a5ea5050ffae6c698a6a769710b6e23212f638f
Niv Sardi <xaiki@evilgiggle.com>
2022-08-22 16:43:27 -0300

    watcher: show seen files
    Signed-off-by: Niv Sardi <xaiki@evilgiggle.com>

--- crawler/watcher.py
+++ crawler/watcher.py
@@ -3,13 +3,15 @@ import inotify.adapters
 from imtool import read_bounding_boxes, crop
 
 def watch(dir):
+    seen = 0
     i = inotify.adapters.Inotify()
     i.add_watch(dir)
     for event in i.event_gen(yield_nones=False):
         (_, type_names, path, filename) = event
 
         if filename.endswith(".png") and type_names[0] in ['IN_CLOSE_WRITE']:
-            print(f"--PATH=[{path}] FILENAME=[{filename}] EVENT_TYPES={type_names}")
+            seen += 1
+            print(f"{seen} PATH=[{path}] FILENAME=[{filename}] EVENT_TYPES={type_names}")
             try:
                 bbs = read_bounding_boxes(os.path.join(path, filename.replace('.png', '.txt')))
                 crop(os.path.join(path, filename), bbs)
