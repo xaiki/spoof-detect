d2498f2c4546961b85de3a9165c9301ebf271410
Niv Sardi <xaiki@evilgiggle.com>
2022-09-23 01:29:27 -0300

    run.sh: support detecting CPUs and add moar icons

--- run.sh
+++ run.sh
@@ -1,13 +1,16 @@
 #!/bin/sh
-
 set -e
 
 PY=python3
+PARALLEL=$(cat /proc/cpuinfo | grep processor | wc -l)
 
+echo "📊 detected ${PARALLEL} cores"
 echo "🏛 fetching entities"
 ${PY} ./python/get_entities.py
 echo "🌏 getting vendor data"
-${PY} ./python/vendor.py --parallel $(cat /proc/cpuinfo | grep processor | wc -l)
+${PY} ./python/vendor.py --parallel $PARALLEL
+echo "☠ getting extra backgrounds from OpenFish"
+${PY} ./python/openfish.py --parallel $PARALLEL
 echo "✨ augmenting data"
 ${PY} ./python/augment.py
 echo "🖼 croping augmented data"
